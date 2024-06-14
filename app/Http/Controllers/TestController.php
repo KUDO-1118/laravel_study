aaa0f6347eebc63fbff0a7cb9ed40585f3350b52
{KUDO} <au4saf010@gmail.com>
2024-06-14 11:18:40 +0900

    Eloquent(エロクアント)

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,7 +9,15 @@ class TestController extends Controller
 {
     public function index ()//indexメソッドを追加
     {
+        //Eloquent(エロクアント)
         $values = Test::all();//全件取得
+
+        $count = Test::count();//件数を取得
+
+        $first = Test::findOrFail(1);//1件取得
+
+        $where = Test::where('text', '=', '');//条件に合うレコードを取得
+        
 
         // dd($values);
     

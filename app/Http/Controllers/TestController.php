c1a8c7cb86fc98f95b9d85585f6053ef81ef3c84
{KUDO} <au4saf010@gmail.com>
2024-06-14 10:17:08 +0900

    モデルをコントローラ内で取得する

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Test;//Testモデルを使えるように読み込む
 
 class TestController extends Controller
 {
     public function index ()//indexメソッドを追加
     {
-        return view('tests.test');// tests(フォルダ名)testファイル名
+        $values = Test::all();//全件取得
+
+        // dd($values);
+    
+        return view('tests.test', compact('values'));//compact関数でView側に変数を渡すと楽
     }
 }

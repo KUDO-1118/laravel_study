0cbda62b84403cfe0f99cc93fe78d2d05e9a534c
{KUDO} <au4saf010@gmail.com>
2024-06-20 09:04:54 +0900

    ダミーデータ(Seeder)

---
+++ database/seeders/TestSeeder.php
@@ -0,0 +1,31 @@
+<?php
+//$ php artisan make:seeder TestSeederで作成
+//シーディング：https://readouble.com/laravel/9.x/ja/seeding.html
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tests')->insert([
+            [
+                'text' => 'aaa',
+                'created_at' => Now()
+            ],
+            [
+                'text' => 'bbb',
+                'created_at' => Now()
+            ]
+        ]);
+    }
+}

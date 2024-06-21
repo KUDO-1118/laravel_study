87e86f3dd33263e6d3a02a79b4863b2ba55b03ba
{KUDO} <au4saf010@gmail.com>
2024-06-21 10:18:56 +0900

    リレーション(1対多)2

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Area extends Model
 {
     use HasFactory;
+
+    public function shops()
+    {
+        return $this->hasMany(Shop::class);//hasMany は複数のレコードを持つ 1対多の関係 を表す この場合は1つのエリアに複数の店舗を持つ という意味 なので hasMany を使う
+    }
 }
--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);// 1対多の関係 1つのエリアに複数の店舗が存在 その逆も同様 なのでbelongsTo(属するという意味) で記述
+    }
 }
